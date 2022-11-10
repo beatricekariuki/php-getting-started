f7857790ecb51d67594e54f63654dcac4312c2bf
beatricekariuki <kariukibetty515@gmail.com>
2022-11-10 13:30:40 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+echo "Hello world program";
+
+$myName = "Betty Kariuki";
+echo $myName;
+var_dump($myName);
+
+// integers
+$a = 30;
+$b = "30";
+$c =true;
+$d =200.34;
+
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+
+echo "My Name is ".$myName. "My M-PESA balance is Ksh. ".$d;
+ 
